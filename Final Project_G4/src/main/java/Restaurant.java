129b6deb8c903c003ef596e7be9b08031170222f
yenrongchen <93334261+yenrongchen@users.noreply.github.com>
2023-05-18 00:00:48 +0800

    Update Restaurant.java

--- Final Project_G4/src/main/java/Restaurant.java
+++ Final Project_G4/src/main/java/Restaurant.java
@@ -1,4 +1,35 @@
 
 public class Restaurant {
+    private int restID;
+    private String name;
+    private double typeWei;
+    private double budWei;
+    private double dTimeWei;
+    private double distWei;
+    private double score;
 
+    public Restaurant(int id, String name, double typeWei, double budWei, double dTimeWei, double distWei) {
+        this.restID = id;
+        this.name = name;
+        this.typeWei = typeWei;
+        this.budWei = budWei;
+        this.dTimeWei = dTimeWei;
+        this.distWei = distWei;
+    }
+
+    public void calScore() {
+
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public int getRestID() {
+        return restID;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
