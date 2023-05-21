668687a370a5bf9431e2fc6263aa7ab7a2a5ac34
yenrong <1040130@gmail.com>
2023-05-21 21:57:46 +0800

    update for user

--- Final Project_G4/src/main/java/Selector.java
+++ Final Project_G4/src/main/java/Selector.java
@@ -13,39 +13,40 @@ public class Selector {
         boolean delDist = distance.remove("不限");
 
         int count = 0;
-        String query = "Select name From Restaurant Where";
+        String query = "SELECT Name FROM Restaurant WHERE";
 
         for (int i = 0; i < type.size(); i++) {
             if (count > 0) {
-                query += " or ";
+                query += " OR ";
             }
             query += "Type = " + type.get(i);
             count++;
         }
 
         for (int i = 0; i < budget.size(); i++) {
-            query += " or ";
+            query += " OR ";
             query += "Budget = " + budget.get(i);
         }
 
         for (int i = 0; i < time.size(); i++) {
-            query += " or ";
+            query += " OR ";
             query += "Time = " + time.get(i);
         }
 
         for (int i = 0; i < distance.size(); i++) {
-            query += " or ";
+            query += " OR ";
             query += "Distance = " + distance.get(i);
         }
 
-        String weekday = "select CONVERT(WEEKDAY(CURDATE()), char)";
+        String weekday = "SELECT CONVERT(WEEKDAY(CURDATE()), char)";
 
-        query += "and (Open1 like \'%" + weekday + "%\' and CURTIME() between " +
-                "CONVERT(substring(Time1, 1, 8), time) and CONVERT(substring(Time1, 10, 8), time)) " +
-                "or (Open2 like \'%" + weekday + "%\' and CURTIME() between " +
-                "CONVERT(substring(Time2, 1, 8), time) and CONVERT(substring(Time2, 10, 8), time)) " +
-                "or (Open3 like \'%" + weekday + "%\' and CURTIME() between " +
-                "CONVERT(substring(Time3, 1, 8), time) and CONVERT(substring(Time3, 10, 8), time))";
+        query += "AND (Open1 LIKE \'%" + weekday + "%\' AND CURTIME() BETWEEN " +
+                "CONVERT(substring(Time1, 1, 8), time) AND CONVERT(substring(Time1, 10, 8), time)) " +
+                "OR (Open2 LIKE \'%" + weekday + "%\' AND CURTIME() BETWEEN " +
+                "CONVERT(substring(Time2, 1, 8), time) AND CONVERT(substring(Time2, 10, 8), time)) " +
+                "OR (Open3 LIKE \'%" + weekday + "%\' AND CURTIME() BETWEEN " +
+                "CONVERT(substring(Time3, 1, 8), time) AND CONVERT(substring(Time3, 10, 8), time));";
 
     }
 }
+
--- Final Project_G4/src/main/java/User.java
+++ Final Project_G4/src/main/java/User.java
@@ -1,4 +1,74 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class User {
+	
+	private int userID;
+	private String type;
+	private String comments;
+	private ArrayList<Integer> collection = new ArrayList<Integer>();
+	
+	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://140.119.203.60:3306/dbms_project?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+	static final String USER = "yenrong";
+	static final String PASS = "dbmsproject";
+	
+	public User(int id, String type) {
+		this.userID = id;
+		this.type = type;
+	}
+	
+	public int getUserID() {
+		return userID;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void Write(Restaurant r) { //time default?
+		try {
+			Connection con = DriverManager.getConnection(DB_URL, USER, PASS);
+			
+			String query = "INSERT INTO Review VALUES(" + userID + ", " + r.getRestID() + ", `" + comments + "`);";
+			
+			Statement stat = con.createStatement();
+			stat.execute(query);
 
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	} 
+	
+	public String collect(Restaurant r) { // time?
+		String result = "";
+		double weight = 1; //預設值
+//		if(有設定權重) {
+//			weight = ;
+//		}
+		
+		try {
+			Connection con = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			if(collection.size() < 3) {
+				String query = "INSERT INTO Collection VALUES(" + userID + ", " + r.getRestID() + ", " + weight + ");";
+				Statement stat = con.createStatement();
+				stat.execute(query);
+				collection.add(r.getRestID());
+				result = "收藏成功!";
+			} else {
+				result = "您的收藏餐廳數已達上限";
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
 }
