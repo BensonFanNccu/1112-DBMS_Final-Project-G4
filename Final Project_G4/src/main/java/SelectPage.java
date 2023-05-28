3a7b52399fb98f927835db7cd879212ba2da7ffe
yenrong <1040130@gmail.com>
2023-05-28 10:07:25 +0800

    update

--- Final Project_G4/src/main/java/SelectPage.java
+++ Final Project_G4/src/main/java/SelectPage.java
@@ -1,5 +1,5 @@
 import java.io.IOException;
-
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,18 +9,24 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/SelectPage")
 public class SelectPage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+	private ArrayList<String> type = new ArrayList<String>();
+	private ArrayList<String> budget = new ArrayList<String>();
+	private ArrayList<String> time = new ArrayList<String>();
+	private ArrayList<String> distance = new ArrayList<String>();
+
 	public SelectPage() {
-	    super();
+		super();
 	}
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setCharacterEncoding("UTF-8");
 		request.setAttribute("user", "?" + request.getQueryString());
 		request.getRequestDispatcher("selectRestaurant.jsp").forward(request, response);
 	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		doGet(request,response);
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
 	}
 }
